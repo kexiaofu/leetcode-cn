fdfc1bfb4fcf4396989fc6c8d6a4c5769962d5d0
liangjf <liangjf@yingmi.cn>
2021-07-22 17:43:52 +0800

    Q: 322

--- 322.零钱兑换.js
+++ 322.零钱兑换.js
@@ -11,6 +11,18 @@
  * @return {number}
  */
 var coinChange = function(coins, amount) {
+  let result = Array(amount + 1).fill(Infinity);
+  result[0] = 0;
+  for (let i = 0, len = coins.length; i < len; i++) {
+    for (let j = 1; j < amount + 1; j++) {
+      if (coins[i] <= j) {
+        // why ?
+        result[j] = Math.min(result[j], result[j - coins[i]] + 1)
+      }
+      // console.log(result)
+    }
+  }
+  return result[amount] !== Infinity ? result[amount] : -1
 
 };
 // @lc code=end
